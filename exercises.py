64de70f0fdad66ed7a84cba829559efd11e08643
Sera De La Rosa <sera.delarosa@outlook.com>
2025-03-28 20:21:21 -0400

    exercises 2 and 3 completed.

--- exercises.py
+++ exercises.py
@@ -1,7 +1,6 @@
-# Step 1: Define a Game class and initialize game state
-
+# Define a Game class and initialize game state
 class Game():
-    def __init__(self, turn, tie, winner, board):
+    def __init__(self):
         # string attribute indicated whose turn it is initialized as X
         self.turn = 'X'
         # a boolean attribute indicating if the game ended in a tie, initialized as False
@@ -14,3 +13,36 @@ class Game():
             'a2': None, 'b2': None, 'c2': None,
             'a3': None, 'b3': None, 'c3': None,
         }
+
+    # define a play_game method 
+    def play_game(self):
+        # confirm the method is accessible on an instance of the Game class
+        print("Welcome to Tic-Tac-Toe! Let's start.")
+    # method to print the current state of the board
+    def print_board(self):
+        b = self.board
+        print(f"""
+            A   B   C
+        1)  {b['a1'] or ' '} | {b['b1'] or ' '} | {b['c1'] or ' '}
+            ----------
+        2)  {b['a2'] or ' '} | {b['b2'] or ' '} | {b['c2'] or ' '}
+            ----------
+        3)  {b['a3'] or ' '} | {b['b3'] or ' '} | {b['c3'] or ' '}
+        """)
+
+    # method to print the current game status
+    def print_message(self):
+        if self.tie:
+            print("Tie game!")
+        elif self.winner:
+            print(f"{self.winner} wins the game!")
+        else:
+            print(f"It's player {self.turn}'s turn!")
+
+     # consolidated render method
+    def render(self):
+        self.print_board()
+        self.print_message()
+
+game_instance = Game()
+game_instance.play_game()
